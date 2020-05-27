04f1c5cc28311a290c20c637d0554bbc8383edda
goard <gokmen.arda@ingka.com>
2020-05-28 00:51:15 +0200

    first commit

---
+++ src/main/java/com/spring/openshift/api/SpringExampleApplication.java
@@ -0,0 +1,31 @@
+package com.spring.openshift.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringExampleApplication {
+
+	@GetMapping("/")
+	public String welcome() {
+
+		return "Welcome to spring boot";
+
+	}
+
+	@GetMapping("/{input}")
+	public String congrats(@PathVariable String input) {
+
+		return "Hi " + input + " Your application is deployed successfully... ";
+
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExampleApplication.class, args);
+	}
+
+}
